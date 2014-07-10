2d70f2fdd4fe8ccb1b5908d32c2a88230a3a6acd
Torey Hickman <torey@toreyhickman.com>
2014-07-10 19:45:21 -0400

    Add conditional around running driver code

--- driver.rb
+++ driver.rb
@@ -1,52 +1,41 @@
-require_relative "grouper"
 require "pp"
 require 'csv'
 require 'dbc-ruby'
 
-Names = [
-    "Andrew",
-    "Anne",
-    "Ariel",
-    "Ben",
-    "Caroline",
-    "Casey",
-    "Daniel",
-    "Dusty",
-    "Beth",
-    "Erik",
-    "Fabi",
-    "Gustavo",
-    "Jaimin",
-    "Jared",
-    "Justin",
-    "Michael",
-    "Mohammad",
-    "Nate",
-    "Oliver",
-    "Paige",
-    "Parth",
-    "Raza",
-    "Robb",
-    "Sammy",
-    "Sasha",
-    "Tom N."
-  ]
-
-  PreviousGroups = [
-    %w(Casey Gustavo Anne Nate),
-    %w(Daniel Tom\ N. Fabi Tom\ H.),
-    %w(Sasha Ariel Erik Jaimin),
-    %w(Sammy Justin Andrew Liz),
-    %w(Mohammad Milan Jared Parth),
-    %w(Dusty Michael Oliver Paige),
-    %w(Ben Beth Caroline Robb),
-  ]
+require_relative "grouper"
+require_relative "dbc_patch"
 
 
-grouper = Grouper.new(list: Names, previous_groups: PreviousGroups)
 
-proposed_groups = grouper.group
 
-puts grouper.report_overlap(proposed_groups)
+DBC::token = ENV["DBC_API_KEY"]
 
-pp proposed_groups
+# Assign ARGV values to variables
+CohortName = ARGV[0]
+
+
+
+
+cohort = DBC::Cohort.find_by_name(CohortName)
+Names = cohort.student_names
+
+PreviousGroups = [
+  %w(Casey Gustavo Anne Nate),
+  %w(Daniel Tom\ N. Fabi Tom\ H.),
+  %w(Sasha Ariel Erik Jaimin),
+  %w(Sammy Justin Andrew Liz),
+  %w(Mohammad Milan Jared Parth),
+  %w(Dusty Michael Oliver Paige),
+  %w(Ben Beth Caroline Robb)
+]
+
+
+if ARGV[0] && __FILE__ == $0
+  grouper = Grouper.new(list: Names, previous_groups: PreviousGroups)
+
+  proposed_groups = grouper.group
+
+  puts grouper.report_overlap(proposed_groups)
+
+  pp proposed_groups
+end
