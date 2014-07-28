f0d4e3376b4ca42e836064f87a211ce220ec233d
Torey Hickman <torey@toreyhickman.com>
2014-07-27 19:46:52 -0500

    Major refactor of Grouper#group

--- grouper.rb
+++ grouper.rb
@@ -9,40 +9,19 @@ class Grouper
 
   def group
     record_pair_history
+    set_groups_structure
 
-    @groups = name_pairs_map.each_with_object(Array.new(1, [])) do |(name, previous_pairs), new_groups|
-
-      new_groups.each do |group|
-        if group.size < max_group_size && (previous_pairs & group).empty?
-          group << name
-          break
-        end
-      end
-
-
-      if new_groups.none? { |group| group.include?(name) }
-        new_groups.size == max_groups ? (new_groups.find { |group| group.size < max_group_size } << name) : (new_groups << [name])
-      end
-    end
-
-
-    if groups.last.size == 1 || max_group_size - groups.last.size > 1
-      extras = groups.pop
-
-      extras.each do |name|
-        groups_without_extras = groups.select { |group| group.size == max_group_size }
-        group_for_name = groups_without_extras.min_by { |group| group & name_pairs_map[name] }
-        group_for_name ? group_for_name << name : groups.first << name
-      end
+    name_pairs_map.each do |name, previous_pairs|
+      place_in_group(name, previous_pairs)
     end
 
     groups
   end
 
   def report_overlap
-    record_of_repeat_pairs = name_pairs_map.keys.sort.each_with_object([]) do |name, memo|
-      overlap = name_pairs_map[name] & groups.find { |group| group.include? name}
-      memo << "#{name}:\t#{overlap.size } repeat pairs" unless overlap.empty?
+    record_of_repeat_pairs = list.each_with_object(Array.new) do |name, overlap_records|
+      overlap = repeat_pairs(name)
+      overlap_records << "#{name}:\t#{overlap.size } repeat pairs" unless overlap.empty?
     end
 
     record_of_repeat_pairs << "No repeat pairs" if record_of_repeat_pairs.empty?
@@ -50,7 +29,7 @@ class Grouper
   end
 
   def group_list
-    groups_made? ? groups.map.with_index { |group, index| "#{index + 1}.  #{group.join(', ')}" } : "Groups haven't been assigned."
+    groups_made? ? stringify_groups : "Groups haven't been assigned."
   end
 
 
@@ -58,17 +37,64 @@ class Grouper
   attr_reader :list, :previous_groups, :name_pairs_map, :max_group_size
 
   def record_pair_history
-    @name_pairs_map = shuffled_list.each_with_object({}) do |list_item, memo|
-      memo[list_item] = previous_pairs(list_item)
+    @name_pairs_map = shuffled_list.each_with_object(Hash.new) do |name, name_pairs|
+      name_pairs[name] = previous_pairs(name)
     end
   end
 
+  def set_groups_structure
+    @groups = Array.new(max_groups) { Array.new(max_group_size, nil) }
+    correct_for_uneven_groups
+    disband_small_final_group
+
+    groups
+  end
+
+  def correct_for_uneven_groups
+    unless even_groups?
+      groups.pop
+      groups << Array.new(uneven_group_count, nil)
+    end
+  end
+
+  def disband_small_final_group
+    if last_group_too_small
+      groups.pop.each_with_index { |name, index| groups[index] << name }
+    end
+  end
+
+  def last_group_too_small
+    last_group_size = groups.last.size
+
+    last_group_size == 1 || (max_group_size - last_group_size) > 1
+  end
+
   def previous_pairs(name)
-    previous_groups.select { |group| group.include?(name) }.flatten.uniq.reject { |x| x == name }
+    previous_groups_of(name).flatten.uniq.reject { |x| x == name }
+  end
+
+  def previous_groups_of(name)
+    previous_groups.select { |group| group.include?(name) }
   end
 
   def max_groups
-    @max_groups ||= (list.size % max_group_size).zero? ? (list.size / max_group_size) : (list.size / max_group_size + 1)
+    @max_groups ||= calculate_max_groups
+  end
+
+  def calculate_max_groups
+    even_groups? ? full_groups_count : full_groups_count + 1
+  end
+
+  def even_groups?
+    uneven_group_count.zero?
+  end
+
+  def uneven_group_count
+    (list.size % max_group_size)
+  end
+
+  def full_groups_count
+    list.size / max_group_size
   end
 
   def shuffled_list
@@ -76,6 +102,33 @@ class Grouper
   end
 
   def groups_made?
-    !groups.nil?
+    !(groups.nil?)
+  end
+
+  def stringify_groups
+    groups.map.with_index { |group, index| "#{index + 1}.  #{group.join(', ')}" }
+  end
+
+  def group_of(name)
+    groups.find { |group| group.include? name }
+  end
+
+  def repeat_pairs(name)
+    name_pairs_map[name] & group_of(name)
+  end
+
+  def open_groups
+    groups.select { |group| group.include? nil }
+  end
+
+  def best_group_based_on(previous_pairs)
+    open_groups.min_by { |group| (group & previous_pairs).size }
+  end
+
+  def place_in_group(name, previous_pairs)
+    best_group = best_group_based_on(previous_pairs)
+    index_of_nil = best_group.index(nil)
+
+    best_group[index_of_nil] = name
   end
 end
--- spec/grouper_spec.rb
+++ spec/grouper_spec.rb
@@ -1,28 +1,11 @@
 require_relative "../grouper.rb"
 
 describe Grouper do
-  let(:list) { %w(a b c d e f) }
-  let(:no_repeats) { %w(a b c d) }
-  let(:repeats) { %w(e f g h) }
-  let(:previous_groups) { [ %w(e f), %w(i j k l)] }
+  let(:list) { %w(a b c d e f g h i j k l) }
+  let(:previous_groups) { Array.new }
   let(:grouper) { Grouper.new(list: list, previous_groups: previous_groups) }
 
-  describe "#record_pair_history" do
-    it "returns each item in list mapped to its previous pairs" do
-      expect(grouper.send(:record_pair_history)['e']).to eq %w(f)
-      expect(grouper.send(:record_pair_history)['a']).to eq %w()
-    end
-  end
-
-  describe "#previous_pairs" do
-    it "finds the previous pairs for an item" do
-      expect(grouper.send(:previous_pairs, "e")).to eq %w(f)
-    end
-  end
-
   describe "#group" do
-    let(:list) { %w(a b c d) }
-
     it "calls for the pair history" do
       allow(grouper).to receive(:name_pairs_map) { Array.new }
       allow(grouper).to receive(:record_pair_history) { nil }
@@ -32,81 +15,93 @@ describe Grouper do
       grouper.group
     end
 
-    it "tries to make groups with no repeat pairs" do
-      allow(grouper).to receive(:name_pairs_map) { { :a => [:b],
-                                                     :b => [:a],
-                                                     :c => [:d],
-                                                     :d => [:c] } }
-      allow(grouper).to receive(:max_group_size) { 2 }
+    it "calls for setting the groups structure" do
+      allow(grouper).to receive(:name_pairs_map) { Array.new }
+      allow(grouper).to receive(:record_pair_history) { nil }
 
-      expect(grouper.group).to eq [[:a, :c], [:b, :d]]
+      expect(grouper).to receive(:set_groups_structure)
+
+      grouper.group
     end
 
-    it "groups according to max group size" do
-      allow(grouper).to receive(:max_group_size) { 3 }
-      allow(grouper).to receive(:name_pairs_map) { { :a => [],
-                                                     :b => [],
-                                                     :c => [],
-                                                     :d => [],
-                                                     :e => [],
-                                                     :f => [] } }
+    it "fills the open slots" do
+      grouper.group
+      nils = grouper.groups.flatten.keep_if(&:nil?)
 
-      group_sizes = grouper.group.map(&:size)
-      expect(group_sizes.all? { |size| size == 3 }).to be true
+      expect(nils).to be_empty
+    end
+  end
+
+  describe "private methods" do
+    describe "#record_pair_history" do
+      let(:previous_groups) { [%w(e f)] }
+
+      it "returns each item in list mapped to its previous pairs" do
+        expect(grouper.send(:record_pair_history)['e']).to eq %w(f)
+        expect(grouper.send(:record_pair_history)['a']).to eq %w()
+      end
     end
 
-    it "adds extras if the size of the last group is two or more less than max group size" do
-      long_list = %w(a b c d e f g h i j)
-      grouper = Grouper.new(list: long_list, previous_groups: previous_groups)
+    describe "#previous_pairs" do
+      let(:previous_groups) { [%w(e f)] }
 
-      allow(grouper).to receive(:max_group_size) { 4 }
-      allow(grouper).to receive(:name_pairs_map) { { :a => [],
-                                                     :b => [],
-                                                     :c => [],
-                                                     :d => [],
-                                                     :e => [],
-                                                     :f => [],
-                                                     :g => [],
-                                                     :h => [],
-                                                     :i => [],
-                                                     :j => [] } }
-
-      group_sizes = grouper.group.map(&:size)
-      expect(group_sizes).to eq [5, 5]
+      it "finds the previous pairs for an item" do
+        expect(grouper.send(:previous_pairs, "e")).to eq %w(f)
+      end
     end
 
-    it "keeps last group if size is one less than max group size" do
-      long_list = %w(a b c d e f g h)
-      grouper = Grouper.new(list: long_list, previous_groups: previous_groups)
+    describe "#max_groups" do
+      context "list size evenly divisible by max group size" do
+        it "divides list size by max group size" do
+          allow(grouper).to receive(:list) { Array.new(16) }
+          allow(grouper).to receive(:max_group_size) { 4 }
 
-      allow(grouper).to receive(:max_group_size) { 3 }
-      allow(grouper).to receive(:name_pairs_map) { { :a => [],
-                                        :b => [],
-                                        :c => [],
-                                        :d => [],
-                                        :e => [],
-                                        :f => [],
-                                        :g => [],
-                                        :h => [] } }
+          expect(grouper.send(:max_groups)).to eq 4
+        end
+      end
 
-      group_sizes = grouper.group.map(&:size)
-      expect(group_sizes).to eq [3, 3, 2]
+      context "list size not evenly divisible by max group size" do
+        it "divides list size by max group size and adds one" do
+          allow(grouper).to receive(:list) { Array.new(18) }
+          allow(grouper).to receive(:max_group_size) { 4 }
+
+          expect(grouper.send(:max_groups)).to eq 5
+        end
+      end
     end
 
-    it "adds extras if last group size is one" do
-      five_item_list = %w(a b c d e)
-      grouper = Grouper.new(list: five_item_list, previous_groups: previous_groups)
+    describe "#set_groups_structure" do
+      context "list size evenly divisible by max group size" do
+        it "creates the right number of groups" do
+          allow(grouper).to receive(:list) { Array.new(16) }
+          allow(grouper).to receive(:max_group_size) { 4 }
 
-      allow(grouper).to receive(:max_group_size) { 2 }
-      allow(grouper).to receive(:name_pairs_map) { { :a => [],
-                                        :b => [],
-                                        :c => [],
-                                        :d => [],
-                                        :e => [] } }
+          expect(grouper.send(:set_groups_structure).size).to eq 4
+        end
 
-      group_sizes = grouper.group.map(&:size)
-      expect(group_sizes).to eq [3, 2]
+        it "puts the correct number of slots in each group" do
+          allow(grouper).to receive(:list) { Array.new(16) }
+          allow(grouper).to receive(:max_group_size) { 4 }
 
+          expect(grouper.send(:set_groups_structure).map(&:size)). to eq [4, 4, 4, 4]
+        end
+      end
+
+      context "list size not evenly divisible by max group size" do
+        it "creates the right number of groups" do
+          allow(grouper).to receive(:list) { Array.new(18) }
+          allow(grouper).to receive(:max_group_size) { 4 }
+
+          expect(grouper.send(:set_groups_structure).size).to eq 4
+        end
+
+        it "puts the correct number of slots in each group" do
+          allow(grouper).to receive(:list) { Array.new(18) }
+          allow(grouper).to receive(:max_group_size) { 4 }
+
+          expect(grouper.send(:set_groups_structure).map(&:size)). to eq [5, 5, 4, 4]
+        end
+      end
     end
   end
 end
